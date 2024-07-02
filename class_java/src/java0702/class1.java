f903df4597dc776c6837dab2b5539e58c8f061b9
PraiseTheSunSola <pledgemo2115@gmail.com>
2024-07-02 10:05:49 +0900

    클래스 공부 시작

---
+++ class_java/src/java0702/class1.java
@@ -0,0 +1,18 @@
+package java0702;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// 메서드 - 귀족된 함수
+		// 함수 - 독립적으로 기능함. 
+		// 클래스는 대문자로 시작함 
+		// 변수 -> 배열 -> 구조체 -> 클래스
+		// new -> 동적할당
+		
+	}
+
+}
+
+class Member{
+	
+}
