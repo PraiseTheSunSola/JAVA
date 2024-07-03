6aaed8846d17b1ce5a6dd8e39191f9254e89d609
PraiseTheSunSola <pledgemo2115@gmail.com>
2024-07-03 09:28:56 +0900

    메서드를 활용하여 코드를 짜야하는 이유 그리고 그 장점들 설명

---
+++ class_java/src/java0703/class3.java
@@ -0,0 +1,14 @@
+package java0703;
+
+public class class3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+// 메서드나 함수를 사용하는 이유 
+// 배열, 계산 - 반복문 - 단순 출력
+//메서드를 사용하면 코드의 재사용과 유지보수, 가독성이 좋아진다.
