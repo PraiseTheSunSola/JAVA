c893c2d6687f795a9626bce926411e1970c9830a
PraiseTheSunSola <pledgemo2115@gmail.com>
2024-07-04 10:45:33 +0900

    과제 풀이 및 메서드 개념 학습

--- class_java/src/java0703/class_homeWork.java
+++ class_java/src/java0703/class_homeWork.java
@@ -1,5 +1,7 @@
 package java0703;
 
+import java.util.Scanner;
+
 public class class_homeWork {
 
 	public static void main(String[] args) {
@@ -17,7 +19,116 @@ public class class_homeWork {
 		  
 		  주사위는 랜덤값이고  사용하는 주사위는 하나.
 		 */
-
+		
+		/* 방식 1
+		Dice game = new Dice();
+		
+		int 석완 = game.getDice("석완");
+		int 형준 = game.getDice("형준");
+		game.result(석완, 형준);
+		*/
+		
+		// 방식 2 
+		DiceGame game = new DiceGame();
+		game.init(); 	// 주사위 게임 초기 설정 - 참가 인원 및 이름 설정
+		game.diceThrow();	 // 참가자 수에 맞게 주사위 던지기 
+		game.result(); 	// 모든 참가자들의 주사위 값 비교 
+		
+		
+		
+		
 	}
+} // end
 
-}
+class DiceGame{
+	String[] names;
+	int[] diceNum;
+	
+	void init() {
+		
+		Scanner sun = new Scanner(System.in);
+		System.out.print("주사위 게임 참가인원 : ");
+		int cnt = sun.nextInt();
+		this.names = new String[cnt];
+		this.diceNum = new int[cnt];
+		
+		sun.nextLine();  	// 버퍼 비우기 - 엔터 
+		for ( int i=0; i<names.length; i++) {
+			System.out.print("참가자 이름 : ");
+			names[i] = sun.nextLine();
+		}
+	}
+	
+	void diceThrow( ) {
+		for( int i=0; i<names.length; i++) {
+			this.diceNum[i] = (int)(Math.random()*6)+1;
+			System.out.println( this.names[i]+ " 주사위 : " + this.diceNum[i]);
+		}
+		
+	}
+	
+	void result() { // 주사위 결과 - 주사위 값이 크면 이긴다. 
+		// 가장 큰 주사위 값 찾기
+		int max= 0;
+		for(int num : diceNum) { 
+			if( max < num) max = num;
+		}
+		
+		// 모든 참가자들의 주사위 값이 같다면 
+		boolean isSame = true;
+		for(int num : diceNum) {
+			if(num != max) isSame = false;
+		}
+		
+		if(isSame) {
+				System.out.println(" 모든 참가자 주사위 값이 같다 - 비김");
+				return; // 데이터 없이 그냥 return 하려면 인스턴스 메서드 타입은 void이여야 한다.
+		}
+		
+
+		// 가장 큰 주사위 값 가지고 있는 사람들이 이긴다. 
+		for( int i=0; i<diceNum.length; i++) {
+			if( diceNum[i] == max) {
+				System.out.println(names[i] + " 승 ");
+			}
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+} // end
+
+
+
+
+
+
+/* 방식 1
+class Dice {
+	
+	void result(int 서, int 전) {
+		
+		if(서 > 전 ) {
+			System.out.println("석완 승, 형준 패 ");
+		} else if (서 < 전) {
+			System.out.println("석완 패, 형준 승");
+		}else 
+			System.out.println("비김");
+		
+	}
+	
+	
+	int getDice( String name) {
+		int num = (int)(Math.random() * 6)+1;
+		System.out.println( name+ " 주사위 :" + num);
+		return num;
+	}
+	
+	
+} //end
+*/
---
+++ class_java/src/java0704/class_Method.java
@@ -0,0 +1,31 @@
+package java0704;
+
+public class class_Method {
+
+	public static void main(String[] args) {
+	
+
+		MethodUtil util = new MethodUtil ();
+		// 두 개의 정수를 반환 하기
+		int[] num = util.twoNum();
+		
+		
+		
+	}
+}
+
+class MethodUtil {
+	
+	int[] twoNum() {
+		int num1 = 20, num2 = 50;
+//		int [] temp = new int [] {num1,num2}; temp 변수를 만들고 return에 temp를 줘도 된다.
+		return new int [] {num1,num2};
+		// return으로 반환 시킬 수 있는 값은 단일 값이다.
+		// 여러개의 데이터를 return 하고 싶다면 배열로 만들어야한다. 
+		// int twoNum() {}; -> int[] twoNum() {}; 로 바꿔줘야한다.
+	}
+	
+	
+	
+	
+}// end
