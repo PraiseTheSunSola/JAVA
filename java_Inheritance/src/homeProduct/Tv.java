6d74b8126fe669f0b0a0b9f0353af70d71196602
PraiseTheSunSola <pledgemo2115@gmail.com>
2024-07-04 15:33:16 +0900

    온오프 표현

---
+++ java_Inheritance/src/homeProduct/Tv.java
@@ -0,0 +1,24 @@
+package homeProduct;
+
+public class Tv {
+
+	boolean onOff;
+	Tv() {
+		this.onOff = false;
+	}
+	
+	void poewr () {		//  tv전원 온 오프
+		
+		this.onOff = !this.onOff;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}// end
