965651ca6628b811068eb7879e1a24f6f8faa9b9
PraiseTheSunSola <pledgemo2115@gmail.com>
2024-07-15 09:11:26 +0900

    Stack 공부 시작

---
+++ java_Collection/src/java0715/Stack.java
@@ -0,0 +1,18 @@
+package java0715;
+
+public class Stack {
+
+	public static void main(String[] args) {
+	
+		/*
+		 			Stack
+		 			
+		 			데이터를 저장하기 위한 입구와 출력하기 위한 출구가 같다.
+		 			한 곳을 통해 저장과 출력이 이루어지는 구조이다.
+		 			FILO - first in last out
+		 			
+		 */
+
+	}
+
+}
