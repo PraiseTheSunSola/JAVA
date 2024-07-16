d7c8a34cc4f98e8103a33478eb14e041b4c78d1f
PraiseTheSunSola <pledgemo2115@gmail.com>
2024-07-16 09:22:09 +0900

    HashMap 공부 시작

---
+++ java_Collection/src/java0716/HashMapStudy1.java
@@ -0,0 +1,21 @@
+package java0716;
+
+public class HashMapStudy1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+/*
+		HashMap ( 이녀석은 Collection interface가 아니고 key와 value로 이루어진 Map interface다. ) 
+		- 순차적으로 저장되지 않는다. 
+		- Key : value 
+		- Key 중복 혀옹 되지 않는다. 
+		-value 중복 허용
+		-Map은 색인 검색에 특화 되어있다.  (색인은 어떤 꾸러미에 들어있는 내용(Database)에 대한 지름길 정보를 가진 일종의 표)
+		-메모리 공간을 많이 소비한다. 
+		
+*/
